fc406fef9fe12ece117e4f8b9e3b4f7c59e70359
CPZhang9487 <chenpozhang@gmail.com>
2026-03-06 23:31:03 +0800

    utils.c 完成 malloc _malloc_r free _free_r 的覆寫

--- CPZhang/utils.c
+++ CPZhang/utils.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#include "FreeRTOS.h"  // 未使用 FreeRTOS 時請自行註解
+
 /* 
  * 重定向 print 輸出到串口
  * 自行替換使用的串口 PRINT_HUART
@@ -15,3 +17,25 @@ int _write(int file, char *ptr, int len) {
 
     return len;
 }
+
+#ifdef PORTABLE_H
+
+#if defined(configUSE_NEWLIB_REENTRANT) && configUSE_NEWLIB_REENTRANT
+void *_malloc_r(struct _reent *r, size_t xSize) {
+    (void)r;
+#else
+void *malloc(size_t xSize) {
+#endif
+    return pvPortMalloc(xSize);
+}
+
+#if defined(configUSE_NEWLIB_REENTRANT) && configUSE_NEWLIB_REENTRANT
+void _free_r(struct _reent *r, void *pv) {
+    (void)r;
+#else
+void free(void *pv) {
+#endif
+    vPortFree(pv);
+}
+
+#endif
